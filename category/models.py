2046c8de11703681ade8ed805a88a78fb831c958
phildens <vborodulin67@2gmail.com>
2024-07-21 23:24:36 +0500

    add right category

---
+++ category/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.name}, {self.category}"
--- products/models.py
+++ products/models.py
@@ -1,20 +1,5 @@
 from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class SubCategory(models.Model):
-    name = models.CharField(max_length=50)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return f"{self.name}, {self.category}"
-
+from category.models import Category, SubCategory
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
