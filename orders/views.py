154628ec17208978e6ad001bde108b302d1296d3
phildens <vborodulin67@2gmail.com>
2024-07-21 15:17:23 +0500

    all orders for user

--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
+from .models import Order, OrderItem, PaymentDetails
+from rest_framework import generics
+from .serializers import OrderSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -13,5 +15,6 @@ from rest_framework.response import Response
 def checkout(request):
     if request.method == 'GET':
         user = request.user
-
-        return Response({'message': 'ok'})
+        order_list = Order.objects.filter(user=user)
+        order_serializer = OrderSerializer(order_list, many=True)
+        return Response(order_serializer.data)
