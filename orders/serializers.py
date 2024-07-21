9b66b3cedb6314107aa2496fc72acd8d5e10891d
phildens <vborodulin67@2gmail.com>
2024-07-21 15:04:51 +0500

    add serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from orders.models import Order
+from orders.models import Order, OrderItem, PaymentDetails
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -13,3 +13,15 @@ class OrderSerializer(serializers.ModelSerializer):
             user=validated_data['user'],
         )
         return order
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class PaymentDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentDetails
+        fields = '__all__'
