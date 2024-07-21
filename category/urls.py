4283181c0628e26039ffd2fc0e7adae928b74ae0
phildens <vborodulin67@2gmail.com>
2024-07-22 00:19:53 +0500

    repair category

--- category/urls.py
+++ category/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CategoryListView, SubCategoryListView
+from .views import CategoryListView, SubCategoryListView, product_in_subcategory, product_in_category
 urlpatterns = [
     path('', CategoryListView.as_view(), name='category-list'),
-    path('subcatgory', SubCategoryListView.as_view(), name='category-sub-list'),
+    path('subcatgory/', SubCategoryListView.as_view(), name='category-sub-list'),
+    path('subcatgory/<int:category_id>/', product_in_subcategory),
+    path('<int:category_id>/', product_in_category),
 ]
--- category/views.py
+++ category/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from itertools import product
 
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+
+from products.serializers import ProductListSerializer
 from category.models import Category, SubCategory
 from category.serializers import CategorySerializer, SubCategorySerializer
+from products.models import Product
 
 
 # Create your views here.
@@ -14,3 +20,10 @@ class CategoryListView(ListAPIView):
 class SubCategoryListView(ListAPIView):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+
+@api_view(['GET'])
+def product_in_subcategory(request, category_id):
+    sub_category = SubCategory.objects.get(pk=category_id)
+    products = Product.objects.filter(sub_category=sub_category)
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
